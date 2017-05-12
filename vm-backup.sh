590baf22aae27ac901c40639b7e2bfb73465197d
Andrew Naoum <andrew@naoum.com>
2017-05-12 17:06:43 +1000

    fix backups for nobackups

--- vm-backup.sh
+++ vm-backup.sh
@@ -30,18 +30,21 @@ if virsh dominfo "$DOMAIN" | grep -q 'State:\s*running'; then
     else
         QUIESCE=""
     fi
-    DISKSPEC=""
-    virsh domblklist "$DOMAIN" --details | sed -n 's/^file *disk *\([^ ]*\) *\(.*\)/\1:\2/p' | grep -v 'nobackup' | while IFS=: read -r target source; do
-        DISKSPEC="$DISKSPEC --diskspec "$target,snapshot=external""
+    DISKSPEC="$(
+    virsh domblklist "$DOMAIN" --details | sed -n 's/^file *disk *\([^ ]*\) *\(.*\)/\1:\2/p' | while IFS=: read -r target source; do
+        echo -n " --diskspec $target,snapshot=external"
     done
+    )"
     virsh snapshot-create-as --domain "$DOMAIN" --name "$SNAPSHOT_NAME-TEMP.qcow2" --no-metadata --atomic $QUIESCE --disk-only $DISKSPEC
-    virsh domblklist "$DOMAIN" --details | sed -n 's/^file *disk *\([^ ]*\) *\(.*\)/\1:\2/p' | grep -v 'nobackup' | while IFS=: read -r target source; do
+    virsh domblklist "$DOMAIN" --details | sed -n 's/^file *disk *\([^ ]*\) *\(.*\)/\1:\2/p' | while IFS=: read -r target source; do
         BACKUP_SRC="$(qemu-img info "$source" | grep '^backing file: *' | sed 's/backing file: *//')"
         BACKUP_DST="$BACKUP_LOCATION/$target.$SNAPSHOT_NAME.qcow2"
-        echo "Copying $BACKUP_SRC to $BACKUP_DST."
-        qemu-img convert -p -O qcow2 "$BACKUP_SRC" "$BACKUP_DST"
-        echo "Changing permissions of $BACKUP_DST to 0400."
-        chmod 0400 "$BACKUP_DST"
+        if [[ "$BACKUP_SRC" != *"nobackup"* ]]; then
+            echo "Copying $BACKUP_SRC to $BACKUP_DST."
+            qemu-img convert -p -O qcow2 "$BACKUP_SRC" "$BACKUP_DST"
+            echo "Changing permissions of $BACKUP_DST to 0400."
+            chmod 0400 "$BACKUP_DST"
+        fi
         echo "Committing from $source down to $BACKUP_SRC."
         virsh blockcommit "$DOMAIN" "$source" --base "$BACKUP_SRC" --pivot > /dev/null
         echo "Deleting temporary snapshot $source."
